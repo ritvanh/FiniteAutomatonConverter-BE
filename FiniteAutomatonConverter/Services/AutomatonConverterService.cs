a85427fe196e56d90a65441c73269691e6ba245b
RITVAN H <ritvan.hajdari@fshnstudent.info>
2023-01-12 22:48:15 +0100

    nfa to dfa almost done

--- FiniteAutomatonConverter/Services/AutomatonConverterService.cs
+++ FiniteAutomatonConverter/Services/AutomatonConverterService.cs
@@ -43,42 +43,85 @@ namespace FiniteAutomatonConverter.Services
 
         public async Task<Automaton> ConvertNfaToDfa(Automaton enfa)
         {
+
+            //var newStates = new Queue<string[]>();
+            //newStates.Enqueue(new string[] { enfa.InitialState });
+
+            //while (newStates.Any())
+            //{
+            //    var currentProcessingState = newStates.Dequeue();
+
+            //    var newInitialStateTransition = new List<KeyValuePair<string, string>>();
+            //    foreach (var state in currentProcessingState)
+            //    {
+            //        var groupedInitialStateTransitions = nfa.States[nfa.InitialState]
+            //        .GroupBy(x => x.Key)
+            //        .ToDictionary(x => x.Key, x => x.Select(kvp => kvp.Value)
+            //        .ToList());
+
+
+            //        foreach (var x in groupedInitialStateTransitions)
+            //        {
+            //            if (x.Value.Count == 1)
+            //            {
+            //                newInitialStateTransition.Add(new KeyValuePair<string, string>(x.Key, x.Value[0]));
+            //            }
+            //            else
+            //            {
+            //                newStates.Enqueue(x.Value.ToArray());
+            //                newInitialStateTransition.Add(new KeyValuePair<string, string>(x.Key, String.Join(",", x.Value)));
+            //            }
+            //        }
+            //    }
+            //    newTransitions.Add(nfa.InitialState, newInitialStateTransition);
+
+            //}
             var nfa = await ConvertEpsilonNfaToNfa(enfa);
-            var newTransitions = new Dictionary<string, List<KeyValuePair<string, string>>>();
-            var newStates = new Queue<string[]>();
-            newStates.Enqueue(new string[] { enfa.InitialState });
+            var newTransitions = new Dictionary<List<string>, List<KeyValuePair <string, List<string>>>>();
+            var newStates = new List<List<string>>();
+            newStates.Add(new List<string>() { nfa.InitialState});
+            var newStatesProcessor = new Queue<List<string>>();
+            newStatesProcessor.Enqueue(new List<string>() { nfa.InitialState });
 
-            while (newStates.Any())
+            while (newStatesProcessor.Any())
             {
-                var currentProcessingState = newStates.Dequeue();
+                var currentState = newStatesProcessor.Dequeue();
 
-                var newInitialStateTransition = new List<KeyValuePair<string, string>>();
-                foreach (var state in currentProcessingState)
+                //finding transitions of current state(s)
+                var currentStateTransitions = new List<KeyValuePair<string, List<string>>>();
+                currentState.ForEach(x =>
                 {
-                    var groupedInitialStateTransitions = nfa.States[nfa.InitialState]
+                    //grouping transitions by input to form lists on same input
+                    var groupedTransitions = nfa.States[x]
                     .GroupBy(x => x.Key)
                     .ToDictionary(x => x.Key, x => x.Select(kvp => kvp.Value)
                     .ToList());
 
-                    
-                    foreach (var x in groupedInitialStateTransitions)
+                    //adding them to current state transitions
+                    foreach(var kvp in groupedTransitions)
                     {
-                        if (x.Value.Count == 1)
+                        //if input already exists, add found states
+                        if (currentStateTransitions.Any(x => x.Key == kvp.Key))
                         {
-                            newInitialStateTransition.Add(new KeyValuePair<string, string>(x.Key, x.Value[0]));
+                            currentStateTransitions.FirstOrDefault(x => x.Key == kvp.Key).Value.AddRange(kvp.Value);
                         }
+                        //else add record for the newly found input
                         else
                         {
-                            newStates.Enqueue(x.Value.ToArray());
-                            newInitialStateTransition.Add(new KeyValuePair<string, string>(x.Key, String.Join(",", x.Value)));
+                            currentStateTransitions.Add(kvp);
                         }
                     }
-                }
-                newTransitions.Add(nfa.InitialState, newInitialStateTransition);
-
+                });
+                //adding found transitions
+                newTransitions.Add(currentState, currentStateTransitions);
+                //adding states to process
+                var newStateLists = currentStateTransitions.Where(x => !newStates.Contains(x.Value)).Select(x=>x.Value).ToList();
+                newStates.AddRange(newStateLists);
+                newStateLists.ForEach(x => newStatesProcessor.Enqueue(x));
             }
 
-            throw new NotImplementedException();
+
+
         }
 
         public Task<Automaton> MinimizeDfa(Automaton dfa)
